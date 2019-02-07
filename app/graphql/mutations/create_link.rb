6e2d1ee50eacd2a2fa18ec151e71c0761581cdac
tommy <kazu9su@gmail.com>
2019-02-06 19:28:10 -0500

    first commit

---
+++ app/graphql/mutations/create_link.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Mutations
+  class CreateLink < BaseMutation
+    argument :description, String, required: true
+    argument :url, String, required: true
+
+    type Types::LinkType
+
+    def resolve(description: nil, url: nil)
+      Link.create!(
+        description: description,
+        url: url,
+      )
+    end
+  end
+end
---
+++ app/graphql/types/link_type.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Types
+  class LinkType < BaseObject
+    field :id, ID, null: false
+    field :created_at, DateTimeType, null: false
+    field :url, String, null: false
+    field :description, String, null: false
+  end
+end
